0fd0a1a64eaf09c62c14ad3b64ee79edf39137fb
birdslisten <justin.schultz37@gmail.com>
2023-08-23 16:39:46 -0700

    added conditions to check local time and apply past, present or future to class' clear

--- Assets/script.js
+++ Assets/script.js
@@ -8,9 +8,9 @@ $(document).ready(function () {
     $('#currentDay').text(currentDate.format('dddd, MMMM, DD'));
     console.log(currentDate);
 
-    $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+    $('.saveBtn').on('click', function () {
+        var text = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id');
         console.log('Show this if saveBtn is working');
 
         // Confirmed this is storing the text and time locally
@@ -21,6 +21,20 @@ $(document).ready(function () {
     function currentTime() {
         var localTime = dayjs().hour();
         console.log('Is this displaying the current hour: ' + localTime);
+
+        $('.time-block').each(function () {
+            var timeBlock = parseInt($(this).attr('id').split('hr')[1]);
+
+            if (timeBlock < localTime) {
+                $(this).addClass('past');
+            }
+            else if (timeBlock === localTime) {
+                $(this).addClass('present');
+            }
+            else {
+                $(this).addClass('future');
+            }
+        })
     }
 
     currentTime();
@@ -28,7 +42,7 @@ $(document).ready(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
+    // function? How can DOM traversal be used to get the 'hour-x' id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
     //
